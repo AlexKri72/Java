331c3d650039badbb74661a94f016e8c146bff72
AlexKri72 <krial11@rambler.ru>
2022-06-21 18:12:46 +0500

    differenciation folder

---
+++ Seminar2/task05.java
@@ -0,0 +1,10 @@
+// У вас есть отсортированный массив, 
+// нужно используя минимальное колличество попыток найти загаданное число
+
+package Seminar2;
+
+public class task05 {
+    public static void main(String[] args) {
+
+    }
+}
