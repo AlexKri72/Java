f5a2b83546db46cd71ccc3dba60f66e34a038b7d
AlexKri72 <krial11@rambler.ru>
2022-06-25 12:53:43 +0500

    task03 ready

--- Seminar2/homework03.java
+++ Seminar2/homework03.java
@@ -7,6 +7,42 @@
 
 package Seminar2;
 
-public class homework03 {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class homework03 {
+    public static void main(String[] args) {
+
+        // МОЖНО ВКЛЮЧИТЬ РУЧНОЙ ВВОД
+        // System.out.print("\n\nЗадайте выражение вида: 2? + ?5 = 69 , а мы попытаемся
+        // его восстановить: ");
+        // Scanner Scanner = new Scanner(System.in);
+        // String word = Scanner.nextLine();
+        // Scanner.close();
+
+        String word = "2? + ?5 = 69"; // задаем выражение, с пробелами или без пробелов - всё равно
+        System.out.println("\n\nЗадано выражение: " + word + " , его решения:");
+
+        word = word.replace("+", "=");
+        String[] words = word.split("="); // получили массив строк вида [2? , ?5 , 69]
+
+        for (int i = 0; i < words.length; i++) { // убираем пробелы
+            words[i] = words[i].replace(" ", "");
+        }
+
+        int count = 0;
+        for (int i = 0; i < 10; i++) { // заменяем "?" числами и проверяем выполнение условия выражения
+            for (int j = 0; j < 10; j++) {
+                if (Integer.valueOf(words[0].replace("?", "" + i))
+                        + Integer.valueOf(words[1].replace("?", "" + j)) == Integer.valueOf(words[2])) {
+                    count += 1;
+                    System.out.println(count + ") " + words[0].replace("?", "" + i) + " + "
+                            + words[1].replace("?", "" + j) + " = " + words[2] + "\n\n");
+                }
+            }
+        }
+        if (count == 0) {
+            System.out.println("Решений нет\n");
+        }
+    }
 }
