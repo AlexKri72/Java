febbd1c37c788f5a79116a9274c1aa412c231f0b
AlexKri72 <krial11@rambler.ru>
2022-07-27 19:55:53 +0500

    task01 is ready

--- Seminar5/Task01/MainWithoutObjekt.java
+++ Seminar5/Task01/MainWithoutObjekt.java
@@ -1,74 +1,208 @@
-/*
- * Жанр
- * Название песни
- * количество прослушиваний
- * Дай мне список популярной музыки для сна 
- * List<Music>
- * - Название
- * - Автор
- * - Количество прослушиваний
- */
 
 package Seminar5.Task01;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class MainWithoutObjekt {
+
     public static void main(String[] args) {
 
-        Map<String, Map<String, Integer>> playList = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-
-            Map<String, Integer> temp = new HashMap<>();
-            temp.put(genStr(), (int) (Math.random() * 1001) + 1);
-            playList.put(genStr(), temp);
-
-        }
+        List<String> genres = CreateGenres();
+        String[] allSongs = new String[] { "Boshki Dymyatsa", "Sinimi Zheltymi Krasnymi", "Highway To Hell", "Sonet",
+                "Rayda", "Since U Been Gone", "La Grange", "My Heart", "Someday", "Shape Of You", "A cap", "Passport",
+                "Controller", "Notebook", "Troubles", "Phone", "Ring", "Glass water", "Lemonade", "A ram zam zam",
+                "MicroWave", "Room", "Alone", "Alies", "Table face", "Door opening", "bedroom", "Bless you",
+                "Young and brave", "Simple", "Programming", "Summer", "poldnik", "Cause", "Sharp", "Exit", "Gift",
+                "Defeat", "Winner", "Victory", "Quiz", "Fifa", "Telega", "Belly", "Knowledge", "AS far as i know",
+                "Celebrate", "Person", "Dream", "English", "Disney", "Silent", "Night", "Tiger", "Amazon", "Fish",
+                "Mac", "MC", "Fair", "Animoto", "Selfish", "Game", "Hi", "Bye", "See you", "Ghost", "Star", "War",
+                "Yandex", "Witcher", "Loser", "End", "Sky", "Cat", "Hand", "Jewelry", "Lunch" };
+        Map<String, Integer> songRates = CreateSongNames(allSongs);
+        Map<String, Map<String, Integer>> playList = FillMap(genres, songRates);
+        playList = Sort(playList);
         System.out.println(playList);
-        Sort(playList);
-        // for (HashMap item:playList) {
-        //
-        // }
-
-    }
-
-    public static String genStr() {
-        int size = (int) (Math.random() * 10) + 5;
-        String genStr = "";
-        for (int i = 0; i < size; i++) {
-
-            genStr = genStr + (char) ((Math.random() * 26) + 97);
-
+        List<String> vipPlayList;
+        System.out.println("\n\n\n\nКакой плейлист Вы хотите создать?");
+        System.out.println("1. Музыка для танцев.");
+        System.out.println("2. Музыка для сна.");
+        System.out.println("3. Музыка для спорта.");
+        System.out.print("Сделайте свой выбор: ");
+        Scanner choice = new Scanner(System.in);
+        switch (choice.nextInt()) {
+            case (1):
+                vipPlayList = ResultPlaylist(playList, genres.get(0), genres.get(1));
+                System.out.println("Популярная музыка для танцев:");
+                System.out.println(vipPlayList);
+                System.out.println(ShakeShake(vipPlayList));
+                break;
+            case (2):
+                vipPlayList = ResultPlaylist(playList, genres.get(3), genres.get(6));
+                System.out.println("Популярная музыка для сна:");
+                System.out.println(vipPlayList);
+                System.out.println(ShakeShake(vipPlayList));
+                break;
+            case (3):
+                vipPlayList = ResultPlaylist(playList, genres.get(5), genres.get(4));
+                System.out.println("Популярная музыка для спорта:");
+                System.out.println(vipPlayList);
+                System.out.println(ShakeShake(vipPlayList));
+                break;
+            default:
+                System.out.println("В следующий раз сделайте корректный выбор!");
         }
-        return genStr;
     }
 
-    public static void Sort(Map<String, Map<String, Integer>> noSorted) {
-        List<String> howManyListened = new ArrayList<>();
-        for (Map.Entry<String, Map<String, Integer>> item : noSorted.entrySet()) {
-            String[] music = item.getValue().keySet().toArray(new String[item.getValue().keySet().size()]);
-            for (String str : music) {
-                System.out.println(str);
-            }
-            int temp = 0;
-            for (int i = 0; i < music.length; i++) {
-                for (int j = i; j < music.length; j++) {
-                    int max = item.getValue().get(music[j]);
-                    if (max < item.getValue().get(music[j])) {
+    public static List<String> CreateGenres() {
 
-                        temp = j;
-                        max = item.getValue().get(music[j]);
+        List<String> genres = new ArrayList<>();
+        genres.add("Pop");
+        genres.add("Hip-Hop");
+        genres.add("Rap");
+        genres.add("Classic");
+        genres.add("Rock");
+        genres.add("DupStep");
+        genres.add("Relax");
+        return genres;
 
-                    } else {
+    }
 
+    public static Map<String, Integer> CreateSongNames(String[] songs) {
+
+        Map<String, Integer> songNames = new HashMap<>();
+        for (int i = 0; i < songs.length; i++) {
+            songNames.put(songs[i], (int) ((Math.random() * 101) + 1));
+        }
+        return songNames;
+
+    }
+
+    public static Map<String, Map<String, Integer>> FillMap(List<String> genres, Map<String, Integer> songRates) {
+
+        Map<String, Map<String, Integer>> playList = new HashMap<>();
+
+        for (int i = 0; i < songRates.size(); i++) {
+
+            List<String> keyList = new ArrayList<>(songRates.keySet());
+            List<Integer> valueList = new ArrayList<>(songRates.values());
+            Map<String, Integer> temp = new HashMap<>();
+
+            List<String> keyPlayList = new ArrayList<>(playList.keySet());
+            temp.put(keyList.get(i), valueList.get(i));
+
+            if (playList.isEmpty()) {
+                playList.put(genres.get((int) (Math.random() * genres.size())), temp);
+            } else {
+                boolean find = false;
+                int forListGenre = (int) (Math.random() * genres.size());
+                for (int j = 0; j < playList.size(); j++) {
+
+                    if (Objects.equals(genres.get(forListGenre), keyPlayList.get(j))) {
+
+                        playList.get(genres.get(forListGenre)).put(keyList.get(i), valueList.get(i));
+                        find = true;
                     }
                 }
-                howManyListened.add(music[temp]);
+                if (!find) {
+                    playList.put(genres.get(forListGenre), temp);
+                }
             }
         }
-        System.out.println(howManyListened);
+        return playList;
+    }
+
+    public static Map<String, Map<String, Integer>> Sort(Map<String, Map<String, Integer>> noSorted) {
+
+        Map<String, Map<String, Integer>> sorted = new LinkedHashMap<>();
+        List<String> stringList = new ArrayList<>(noSorted.keySet());
+        int count = 0;
+        for (Map.Entry<String, Map<String, Integer>> item : noSorted.entrySet()) {
+
+            List<String> listOfKeys = new ArrayList<>(item.getValue().keySet());
+            List<Integer> listOfValues = new ArrayList<>(item.getValue().values());
+            Map<String, Integer> songs = new LinkedHashMap<>(SortLists(listOfKeys, listOfValues));
+            List<String> newListOfKeys = new ArrayList<>(songs.keySet());
+            List<Integer> newListOfValues = new ArrayList<>(songs.values());
+            if (sorted.containsKey(stringList.get(count))) {
+                sorted.get(stringList.get(count)).put(newListOfKeys.get(count), newListOfValues.get(count));
+            } else {
+                sorted.put(stringList.get(count), songs);
+            }
+
+            count++;
+        }
+        return sorted;
+    }
+
+    public static Map<String, Integer> SortLists(List<String> keys, List<Integer> values) {
+
+        Map<String, Integer> result = new LinkedHashMap<>();
+        List<String> newKeys = new ArrayList<>();
+        List<Integer> newValues = new ArrayList<>();
+
+        while (!values.isEmpty()) {
+
+            int max = values.get(0);
+            int indexMax = 0;
+
+            for (int i = 0; i < values.size(); i++) {
+
+                if (max < values.get(i)) {
+                    max = values.get(i);
+                    indexMax = i;
+                }
+            }
+            newValues.add(max);
+            newKeys.add(keys.get(indexMax));
+            values.remove(indexMax);
+            keys.remove(indexMax);
+        }
+
+        for (int i = 0; i < newValues.size(); i++) {
+            result.put(newKeys.get(i), newValues.get(i));
+
+        }
+        return result;
+    }
+
+    public static List<String> ResultPlaylist(Map<String, Map<String, Integer>> MusicData, String genre1,
+            String genre2) {
+
+        Map<String, Integer> hipHop = new LinkedHashMap<>(MusicData.get(genre1));
+        List<String> listKeysHipHop = new ArrayList<>(hipHop.keySet());
+        List<Integer> listValuesHipHop = new ArrayList<>(hipHop.values());
+
+        Map<String, Integer> pop = new LinkedHashMap<>(MusicData.get(genre2));
+        List<String> listKeysPop = new ArrayList<>(pop.keySet());
+        List<Integer> listValuesPop = new ArrayList<>(pop.values());
+
+        List<String> result = new ArrayList<>();
+
+        if (listValuesHipHop.get(0) >= listValuesPop.get(0)) {
+            for (int i = 0; i < 5; i++) {
+
+                result.add(listKeysHipHop.get(i));
+                result.add(listKeysPop.get(i));
+
+            }
+        } else {
+            for (int i = 0; i < 5; i++) {
+
+                result.add(listKeysPop.get(i));
+                result.add(listKeysHipHop.get(i));
+
+            }
+        }
+        return result;
+    }
+
+    public static List<String> ShakeShake(List<String> sorted) {
+
+        List<String> shaked = new ArrayList<>();
+        while (!sorted.isEmpty()) {
+            int random = (int) (Math.random() * sorted.size());
+            shaked.add(sorted.get(random));
+            sorted.remove(random);
+        }
+        return shaked;
     }
 }
